e84b0ec57e4eebc0569781bede7a011b54231741
stanchevivan <stanchev.ivan@abv.bg>
2019-10-28 15:58:33 +0200

    - update solution structure (#1)
    - add API project
    - add Mock API project
    - remove unnecessary files

---
+++ LPHotels.Automation/Support/Hooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Fourth.Automation.Framework.Core;
+using Fourth.Automation.Framework.Mobile.Resolvers;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace LPHotels.Automation.Support
+{
+    [Binding]
+    public class Hooks
+    {
+        private IWebDriver driver;
+        private ScenarioContext scenarioContext;
+
+        public Hooks(IWebDriver driver, ScenarioContext scenarioContext)
+        {
+            this.driver = driver;
+            this.scenarioContext = scenarioContext;
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            DriverFactory.Resolvers.Add(new AndroidResolver());
+            DriverFactory.Resolvers.Add(new IOSResolver());
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
