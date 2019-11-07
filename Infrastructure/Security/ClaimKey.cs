00a0e6033127201cef74f48343d197693d2134d4
RasoulFourth <Rasoul.khosravi@fourth.com>
2019-11-07 14:26:46 +0200

    token generator (#2)

---
+++ Infrastructure/Security/ClaimKey.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Security
+{
+    internal class ClaimKey
+    {
+        public const string UserId = "userId";
+        public const string Subdomain = "subdomain";
+    }
+}
---
+++ Infrastructure/Security/TokenGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure.Security
+{
+    public static class TokenGenerator
+    {
+        public static string Get(string subdomain, int userId)
+        {
+            return new JwtGenerator(new JwtConfiguration())
+                .Generate(new Dictionary<string, object>
+                    { { ClaimKey.Subdomain, subdomain }, { ClaimKey.UserId, userId } });
+        }
+    }
+}
