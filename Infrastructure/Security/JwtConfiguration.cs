b5c1aa5d8258b3b54373bd461f211ce2d95d5feb
RasoulFourth <Rasoul.khosravi@fourth.com>
2019-11-07 14:26:46 +0200

    token generator (#2)

---
+++ Infrastructure/Security/JwtConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace Infrastructure.Security
+{
+    internal class JwtConfiguration
+    {
+        public string Secret => ConfigurationManager.AppSettings["jwt_shared_secret"];
+    }
+}
