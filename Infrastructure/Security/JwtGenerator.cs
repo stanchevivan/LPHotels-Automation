4326a7d17048c10a0b58534d8988b6270aae3d4b
RasoulFourth <rasoul.khosravi@fourth.com>
2019-11-07 13:49:03 +0200

    token generator

---
+++ Infrastructure/Security/JwtGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Security
+{
+    internal class JwtGenerator 
+    {
+        private readonly SigningCredentials _signingCredentials;
+        private readonly JwtSecurityTokenHandler _tokenHandler;
+
+        public JwtGenerator(JwtConfiguration jwtConfiguration)
+        {
+            _signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfiguration.Secret)),
+                SecurityAlgorithms.HmacSha256Signature);
+
+            _tokenHandler = new JwtSecurityTokenHandler();
+        }
+
+
+        public string Generate(IDictionary<string, object> claims)
+        {
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = _signingCredentials,
+                Subject = new ClaimsIdentity(claims.Select(x => new Claim(x.Key, x.Value.ToString())))
+            };
+
+
+            return _tokenHandler.CreateEncodedJwt(tokenDescriptor);
+        }
+    }
+}
---
+++ Tests.API/ExampleOfUsingToken.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Infrastructure.Security;
+
+namespace Tests.API
+{
+    //TODO Remove me
+    public class ExampleOfUsingToken
+    {
+        public async Task Test()
+        {
+            var token = TokenGenerator.Get("standaloneqa", 2);
+
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await
+                client.GetAsync($"http://schedulingapiqa.teamhours.com:60473/locations/2/departments/2/from/2019-09-09/to/2019-09-12/labour-demand");
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
