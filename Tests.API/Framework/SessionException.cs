b69e2485a81bb2139ac8cf50a77980b22369b431
VTsvetkova <vera.tsvetkova@fourth.com>
2019-11-07 16:22:54 +0200

    add definitions

---
+++ Tests.API/Framework/SessionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.API.Framework
+{
+    public class SessionException : SystemException
+    {
+        public SessionException(string key, ArgumentException inner)
+        {
+            Message = $"{inner.Message} \nkey = {key}";
+        }
+
+        public SessionException(string key, KeyNotFoundException inner)
+        {
+            Message = $"{inner.Message} \nkey = {key}";
+        }
+
+        public override string Message { get; }
+    }
+}
---
+++ Tests.API/Generators/LocationEntityGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Fourth.TestTools.Generators;
+using TeamHours.DomainModel;
+using Bogus;
+
+namespace Tests.API.Generators
+{
+    class LocationEntityGenerator : BaseGenerator<Location>
+    {
+        public static readonly string CustomerCanonicalId = ConfigurationManager.AppSettings["CustomerCanonicalId"];
+
+        protected override IEnumerable<Location> BuildModels(int count)
+        {
+            var locationFaker = new Faker<Location>().Rules((f, l) =>
+            {
+                l.Code = f.Database.Random.AlphaNumeric(10);
+                l.Name = f.Name.Random.AlphaNumeric(7);
+                l.Latitude = f.Random.Double();
+                l.Longitude = f.Random.Double();
+            });
+
+            return locationFaker.Generate(count);
+        }
+    }
+}
