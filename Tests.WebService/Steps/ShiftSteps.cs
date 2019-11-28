1b0efd28e8bc0ffc4270ad1212d4102ea1c1016f
Denitsa Kulezich <Denitsa.Kulezich@fourth.com>
2019-11-28 15:30:40 +0200

    Added ShitfsFeature

--- Tests.WebService/Steps/ShiftSteps.cs
+++ Tests.WebService/Steps/ShiftSteps.cs
@@ -1,5 +1,8 @@
-﻿using Fourth.Automation.Framework.RestApi.Steps;
+﻿using DataSeeding.Models;
+using Fourth.Automation.Framework.RestApi.Extensions;
+using Fourth.Automation.Framework.RestApi.Steps;
 using Newtonsoft.Json;
+using NUnit.Framework;
 using TechTalk.SpecFlow;
 
 namespace Tests.WebService.Steps
@@ -7,22 +10,32 @@ namespace Tests.WebService.Steps
     [Binding]
     internal class ShiftSteps
     {
+        private readonly ScenarioContext context;
         private readonly RestSession restSession;
 
-        public ShiftSteps(RestSession restSession)
+        public ShiftSteps(ScenarioContext context, RestSession restSession)
         {
+            this.context = context;
             this.restSession = restSession;
         }
 
         [Given(@"request has a shift as a body")]
         public void GivenRequestHasAShiftAsABody()
         {
-            restSession.Request.AddJsonBody(JsonConvert.SerializeObject(new object()));
+            restSession.Request.AddJsonBody(JsonConvert.SerializeObject(context.Get<CreateShiftModel>()));
         }
 
         [Then(@"the shift is created")]
         public void ThenTheShiftIsCreated()
         {
+            var restResponse = restSession.Response;
+            var expectedShift = context.Get<CreateShiftModel>();
+
+            Assert.Multiple(()=> 
+            {
+                Assert.AreEqual(expectedShift.RoleId, restResponse.SelectTokens("RoleId"), "Wrong RoleId");
+                Assert.AreEqual(expectedShift.EmployeeId, restResponse.SelectTokens("EmployeeId"), "Wrong employeeId");
+            });
         }
     }
 }
