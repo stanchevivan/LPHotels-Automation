9df6ba6997093050290db7faf7cf331b6dbe51d1
Denitsa Kulezich <Denitsa.Kulezich@fourth.com>
2019-11-29 15:37:02 +0200

    Cleaned the usings

--- DataSeeding/Hooks/ShiftsHooks.cs
+++ DataSeeding/Hooks/ShiftsHooks.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Common;
+﻿using Common;
 using DataSeeding.Framework;
 using DataSeeding.Generators;
 using DataSeeding.Infrastructure;
@@ -16,7 +11,6 @@ namespace DataSeeding.Hooks
     [Binding]
     public class ShiftsHooks
     {
-
         private readonly ILpHotelsMainUnitOfWork lpHotelsMainUnitOfWork;
         private readonly ScenarioContext context;
 
